9cb784d5054d173cf46f0fe83137cf2acc4b98e1
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-11-22 19:58:58 -0300

    feat: phpmyadmin + Migrations básicas

--- backend/config/database.php
+++ backend/config/database.php
@@ -126,7 +126,7 @@ return [
     */
 
     'migrations' => [
-        'table' => 'migrations',
+        'table' => 'app_migration',
         'update_date_on_publish' => true,
     ],
 
@@ -147,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
